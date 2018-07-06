156b6a7c6c33ad7aad3d5412740640af5efa9a95
root <rewreu@gmail.com>
2018-07-05 22:29:14 -0400

    isolated attacking algorithm

---
+++ Algorithm.py
@@ -0,0 +1,20 @@
+def i_fgsm(steps, model, criterion, Variable, x, y, eps, alpha):
+    for i in range(steps):
+        fc_out = model(Variable)
+        loss = criterion(fc_out, y)
+        loss.backward()
+        Variable.data = alpha*torch.sign(Variable.grad.data)+Variable.data
+        Variable.data = torch.where(Variable.data>x+eps, x+eps, Variable.data)
+        Variable.data = torch.where(Variable.data<x-eps, x-eps, Variable.data)
+        Variable.data = torch.clamp(Variable.data, 0, 1)
+        Variable.grad.data.fill_(0)
+    return Variable
+
+def fgsm(steps, model, criterion, Variable, x, y, eps, alpha):
+    fc_out = model(Variable)
+    loss = criterion(fc_out, y)
+    loss.backward()
+    Variable.data = int(np.sign(alpha))*eps*torch.sign(Variable.grad.data)+Variable.data
+    Variable.data = torch.clamp(Variable.data, 0, 1)
+    Variable.grad.data.fill_(0)
+    return Variable
