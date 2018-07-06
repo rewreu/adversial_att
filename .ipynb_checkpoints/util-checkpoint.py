1706488202e7e4943b00ce4971fa27cbc1241c39
root <rewreu@gmail.com>
2018-07-06 13:34:47 -0400

    further simplification

--- .ipynb_checkpoints/util-checkpoint.py
+++ .ipynb_checkpoints/util-checkpoint.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import torch
+
 def show(original, perturbed):
     # plot the orginal image and attacked image
     plt.figure(figsize=(12,5))
@@ -10,3 +12,8 @@ def show(original, perturbed):
     out_img = out_img.transpose(0,2).transpose(0,1).cpu().detach().numpy()
     plt.subplot(122)
     plt.imshow(out_img)
+
+def one_hot(inx):
+    label = torch.zeros([1,1000]).cuda()
+    label[0, inx] = 1.0
+    return label
