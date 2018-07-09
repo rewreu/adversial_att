e20e5d48e069cfdc2ecaeb0e8fdb6dfdc4a699c8
root <rewreu@gmail.com>
2018-07-08 20:41:41 -0400

    further simplification

--- Algorithm.py
+++ Algorithm.py
@@ -1,16 +1,5 @@
 import torch
 import numpy as np
-def i_fgsm(steps, model, criterion, variable, x, y, eps, alpha):
-    for i in range(steps):
-        fc_out = model(variable)
-        loss = criterion(fc_out, y)
-        loss.backward()
-        variable.data = alpha*torch.sign(variable.grad.data)+variable.data
-        variable.data = torch.where(variable.data>x+eps, x+eps, variable.data)
-        variable.data = torch.where(variable.data<x-eps, x-eps, variable.data)
-        variable.data = torch.clamp(variable.data, 0, 1)
-        variable.grad.data.fill_(0)
-    return variable
 
 def fgsm(steps, model, criterion, variable, x, y, eps, alpha):
     """
@@ -24,26 +13,32 @@ def fgsm(steps, model, criterion, variable, x, y, eps, alpha):
     variable.grad.data.fill_(0)
     return variable
 
+def i_fgsm(steps, model, criterion, variable, x, y, eps, alpha):
+    for i in range(steps):
+        fc_out = model(variable)
+        loss = criterion(fc_out, y)
+        loss.backward()
+        variable.data = alpha*torch.sign(variable.grad.data)+variable.data
+        variable.data = torch.where(variable.data>x+eps, x+eps, variable.data)
+        variable.data = torch.where(variable.data<x-eps, x-eps, variable.data)
+        variable.data = torch.clamp(variable.data, 0, 1)
+        variable.grad.data.fill_(0)
+    return variable
+
 def deepfool(steps, model, criterion, variable, x, y, eps, alpha):
-    num_classes=10
-    overshoot=0.02
-    image = variable.clone()
-    f_image = model.forward(image).data.cpu().numpy().flatten()
+    num_classes, overshoot = 10, 0.02
+    f_image = model.forward(x).data.cpu().numpy().flatten()
     I = (np.array(f_image)).flatten().argsort()[::-1]
     I = I[0:num_classes]
     label = I[0]
-
-    pert_image = image.clone()
-    w = np.zeros(image.shape)
-    r_tot = np.zeros(image.shape)
+    w , r_tot= [np.zeros(x.shape)]*2
     
     x0 = torch.tensor(variable.data).requires_grad_()
     fs = model.forward(x0)
     fs_list = [fs[0,I[k]] for k in range(num_classes)]
     k_i = label
 
-    loop_i = 0
-    while k_i == label and loop_i < steps:
+    for i in range(steps):
         pert = np.inf
         fs[0, I[0]].backward(retain_graph=True)
         grad_orig = x0.grad.data.cpu().numpy().copy()
@@ -51,22 +46,17 @@ def deepfool(steps, model, criterion, variable, x, y, eps, alpha):
             x0.grad.data.fill_(0)
             fs[0, I[k]].backward(retain_graph=True)
             cur_grad = x0.grad.data.cpu().numpy().copy()
-            # set new w_k and new f_k
-            w_k = cur_grad - grad_orig
+            w_k = cur_grad - grad_orig # set new w_k and new f_k
             f_k = (fs[0, I[k]] - fs[0, I[0]]).data.cpu().numpy()
             pert_k = abs(f_k)/np.linalg.norm(w_k.flatten())
-            # determine which w_k to use
             if pert_k < pert:
                 pert = pert_k
                 w = w_k
-        # compute r_i and r_tot
-        # Added 1e-4 for numerical stability
-        r_i =  (pert+1e-4) * w / np.linalg.norm(w)
+        r_i =  (pert+1e-4) * w / np.linalg.norm(w) #1e-4 for numerical stability
         r_tot = np.float32(r_tot + r_i)
-        pert_image = image + (1+overshoot)*torch.from_numpy(r_tot).cuda()
-        x0 = torch.tensor(pert_image.data).requires_grad_()
+        variable = x + (1+overshoot)*torch.from_numpy(r_tot).cuda()
+        x0 = torch.tensor(variable.data).requires_grad_()
         fs = model.forward(x0)
         k_i = np.argmax(fs.data.cpu().numpy().flatten())
-        loop_i += 1
-    r_tot = (1+overshoot)*r_tot
-    return pert_image
+        if k_i!= label: break
+    return variable
