01ca056726592993054a719eb8fd801a8ec3c740
root <rewreu@gmail.com>
2018-07-13 16:54:10 -0400

    use permute to swap the axis instead of transpose

--- Baseline/util.py
+++ Baseline/util.py
@@ -5,11 +5,9 @@ def show(original, perturbed):
     # plot the orginal image and attacked image
     plt.figure(figsize=(12,5))
     plt.subplot(121)
-    orr_img = original[0]
-    orr_img = orr_img.transpose(0,2).transpose(0,1).cpu().detach().numpy()
+    orr_img = original[0].permute(1,2,0).cpu().detach().numpy()
     plt.imshow(orr_img)
-    out_img = perturbed[0]
-    out_img = out_img.transpose(0,2).transpose(0,1).cpu().detach().numpy()
+    out_img = perturbed[0].permute(1,2,0).cpu().detach().numpy()
     plt.subplot(122)
     plt.imshow(out_img)
 
